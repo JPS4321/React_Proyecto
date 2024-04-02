64809209cc9dad4aea39ad609cb9d741a4005d99
MENENDEZGUERRA <menendezguerrajuan@gmail.com>
2024-04-02 12:05:51 -0600

    Adición del componente marquee, texto infinito

---
+++ DivinoSeas_React_Components/src/Marquee.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Marquee.css';
+
+const Marquee = ({ text }) => {
+  return (
+    <div className="marquee-container">
+      <div className="marquee-text">
+        {text}
+      </div>
+    </div>
+  );
+};
+
+export default Marquee;

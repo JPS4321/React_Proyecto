edef530937bfe5c1f353d7cad6934c8f5fd7da29
JPS4321 <sol22102@uvg.edu.gt>
2024-04-02 12:18:47 -0600

    Funcionamiento Search Bar

--- DivinoSeas_React_Components/src/Components/NavBar/Navbar.jsx
+++ DivinoSeas_React_Components/src/Components/NavBar/Navbar.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./Navbar.css"
 import logo from "../../assets/Logo.png"
 import search_Icon from "../../assets/search-b.png"
 
 
 const Navbar = () => {
+    const [val, setVal] = useState('Search');
+
   return (
     <div className='navbar'>
         <img src={logo} alt="DivinoSeas Logo" className='logo'/>
         <ul>
             <li>Home</li>
             <li>About</li>
+            <li>Contact</li>
             <li>Shop Women</li>
             <li>Shop Men</li>
             <li>Shop Couple</li>
         </ul>
-        
+
         <div className='search-box'>
-            <input type="text" placeholder="Search"/>
+            <input type="text" placeholder={val}/>
             <img src= {search_Icon} alt="" />
         </div>
 

640db0466fa097101c1ca126fdab79fc1e344fdf
JPS4321 <sol22102@uvg.edu.gt>
2024-04-17 07:47:01 -0600

    incorporacion de rutas

--- DivinoSeas_React_Components/src/App.jsx
+++ DivinoSeas_React_Components/src/App.jsx
@@ -1,36 +1,15 @@
-import React from 'react'
-import Navbar from './Components/NavBar/Navbar'
-import Marquee from './Components/Marquee/Marquee'
-import ProductCard from './Components/ProductCard/ProductCard'
-import HeroSection0 from './Components/HeroSections/HeroSection0/HeroSection0'
-import Footer from './Components/Footer/Footer'
-import place from './assets/placeholder0.png'
-import bg from './assets/bg-placeholder0.jpg'
+import MainPage from './pages/MainPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
 
 const App = () => {
   return (
-    
-    <div className='container'>
-      <Marquee text='Welcome to Divino Seas'/>y
-      <div className='subheader'>
-      <Navbar/>
-      </div>
-      <HeroSection0 backgroundImage={bg} buttonText="Shop Now"/>
-
-      <div className='grid-container'>
-        <ProductCard imageSrc={place} title="Place Holder 1" price="100" />
-        <ProductCard imageSrc={place} title="Place Holder 2" price="100" />
-        <ProductCard imageSrc={place} title="Place Holder 3" price="100" />
-        <ProductCard imageSrc={place} title="Place Holder 4" price="100" />
-        <ProductCard imageSrc={place} title="Place Holder 5" price="100" />
-        <ProductCard imageSrc={place} title="Place Holder 6" price="100" />
-        <ProductCard imageSrc={place} title="Place Holder 7" price="100" />
-        <ProductCard imageSrc={place} title="Place Holder 8" price="100" />
-        <ProductCard imageSrc={place} title="Place Holder 9" price="100" />
-      </div>
-
-      <Footer/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </Router>
   )
 }
 

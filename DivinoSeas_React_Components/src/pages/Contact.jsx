5b6a2dde59c8302f49db0694475d9942e5da7ca1
JPS4321 <sol22102@uvg.edu.gt>
2024-05-06 20:41:40 -0600

    Actualizacion de Contact Us con el form

--- DivinoSeas_React_Components/src/pages/Contact.jsx
+++ DivinoSeas_React_Components/src/pages/Contact.jsx
@@ -13,7 +13,20 @@ function Contact() {
             <div className="contact-section">
                 <h2>GET IN TOUCH</h2>
                 <p>We would love to hear from you.</p>
-                
+                <form>
+                    <div className="input-flex-container">
+                        <input type="text" name="name" placeholder="Nombre" aria-label="Nombre" />
+                        <input type="email" name="email" placeholder="Correo electrónico" aria-label="Correo electrónico" />
+                    </div>
+                    <textarea name="message" placeholder="Mensaje" aria-label="Mensaje"></textarea>
+                    <div className="checkbox-container">
+                        <input type="checkbox" id="save-info" name="save-info" aria-label="Guardar información" />
+                        <label htmlFor="save-info">
+                            Guarde mi nombre, correo electrónico y sitio web en este navegador para la próxima vez que comente.
+                        </label>
+                    </div>
+                    <button type="submit" className="submit-button">ENVIAR AHORA</button>
+                </form>
             </div>
         </div>
     );

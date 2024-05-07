ae00581140c3bb20338daac685fa36463389149e
JPS4321 <sol22102@uvg.edu.gt>
2024-05-06 20:31:28 -0600

    Creacion base Contact us

--- DivinoSeas_React_Components/src/pages/Contact.jsx
+++ DivinoSeas_React_Components/src/pages/Contact.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import Navbar from '../Components/NavBar/Navbar';
 import Marquee from '../Components/Marquee/Marquee';
-import './pages_css/Collections.css';
+import './pages_css/Contact.css';
 
-
-function Collections() {
-
-
+function Contact() {
     return (
-        <div className='container'>
-      <Marquee text='Welcome to Divino Seas'/>
-      <div className='subheader'>
-      <Navbar/>
-      </div>
-
+        <div className='page-container'>
+            <Marquee text='Welcome to Divino Seas'/>
+            <div className='navigation-bar'>
+                <Navbar/>
+            </div>
+            <div className="contact-section">
+                <h2>GET IN TOUCH</h2>
+                <p>We would love to hear from you.</p>
+                
+            </div>
         </div>
     );
 }
 
-export default Collections;
+export default Contact;

ffee6e91f162c45ba65aa78f4f7647de440af6a5
Joaquin Campos <98112929+JoaquinCampos22155@users.noreply.github.com>
2024-05-09 16:58:19 -0600

    correcciones componentes stat, nav admin

--- DivinoSeas_admin/src/AppAdmin/Componentsadmins/Navbar/Navbar.jsx
+++ DivinoSeas_admin/src/AppAdmin/Componentsadmins/Navbar/Navbar.jsx
@@ -1,27 +1,25 @@
 import React, {useState} from 'react';
 import "./Navbar.css";
 import logo from '../../../assets/Logo.png';
-import search_Icon from '../../../assets/search-b.png';
+import usuario from '../../../assets/usuario.png';
 
 
 const Navbar = () => {
     const [val, setVal] = useState('Search');
 
   return (
-    <div className='navbar2'>
+    <div className='navbar'>
         <ul>
             <li>Inicio</li>
             <li>Inventario</li>
             <li>Ventas</li>
             <li>Ajustes</li>
-            <li>Shop Couple</li>
         </ul>
         <div>
             <img src={logo} alt="DivinoSeas Logo" className='logo'/>
         </div>
-        <div className='search-box'>
-            <input type="text" placeholder={val}/>
-            <img src= {search_Icon} alt="" />
+        <div>
+            <img src={usuario} alt="DivinoSeas Usuario" className='usuario' title='Iniciar sesión'/>
         </div>
 
     </div>
--- DivinoSeas_admin/src/AppAdmin/Componentsadmins/Statbar/Statbar.jsx
+++ DivinoSeas_admin/src/AppAdmin/Componentsadmins/Statbar/Statbar.jsx
@@ -1,29 +1,25 @@
 import "./Statbar.css"; 
 import React, { useState } from 'react';
 import filtroS from '../../../assets/filtro.png'; 
-
+import search_Icon from '../../../assets/search-b.png';
 
 
 const Statbar = () => {
     const [val, setVal] = useState('Search');
   return (
     <div className='statbar'>
-        <ul>
-            <li>Todo</li>
-            <li>Enviado</li>
-            <li>Pendiente</li>
-        </ul>
         <div>
-        <h1 className="titulo-ventas">Ventas</h1>
+            <h4>AGREGAR NUEVO ESTILO</h4>
         </div>
-        
+        <div className='search-box2'>
+            <input type="text" placeholder={val} className='input'/>
+            <img src= {search_Icon} alt="" className='img'/>
+        </div>
         <div>
-            <img src= {filtroS} alt="" className='filtro'/>
+            <h1 style={{ color: '#16d8e8' }}>INVENTARIO</h1>
         </div>
         <div className="statbar">
-            <ul>
-                <li>Reporte Personalizado</li>
-            </ul>
+            <h4>REPORTE DE ENTRADAS Y SALIDAS</h4>
         </div>
         
     </div>

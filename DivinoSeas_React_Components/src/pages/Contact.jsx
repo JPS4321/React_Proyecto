2e508e29ce98e89425d82d9124295b175bba8a43
JPS4321 <sol22102@uvg.edu.gt>
2024-05-09 19:49:27 -0600

    Creacion Contact Section Component e implementacion

--- DivinoSeas_React_Components/src/pages/Contact.jsx
+++ DivinoSeas_React_Components/src/pages/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../Components/NavBar/Navbar';
 import Marquee from '../Components/Marquee/Marquee';
+import ContactSec from '../Components/ContactsSec/ContactsSec';
 import './pages_css/Contact.css';
 
 function Contact() {
@@ -10,24 +11,8 @@ function Contact() {
             <div className='navigation-bar'>
                 <Navbar/>
             </div>
-            <div className="contact-section">
-                <h2>GET IN TOUCH</h2>
-                <p>We would love to hear from you.</p>
-                <form>
-                    <div className="input-flex-container">
-                        <input type="text" name="name" placeholder="Nombre" aria-label="Nombre" />
-                        <input type="email" name="email" placeholder="Correo electrónico" aria-label="Correo electrónico" />
-                    </div>
-                    <textarea name="message" placeholder="Mensaje" aria-label="Mensaje"></textarea>
-                    <div className="checkbox-container">
-                        <input type="checkbox" id="save-info" name="save-info" aria-label="Guardar información" />
-                        <label htmlFor="save-info">
-                            Guarde mi nombre, correo electrónico y sitio web en este navegador para la próxima vez que comente.
-                        </label>
-                    </div>
-                    <button type="submit" className="submit-button">ENVIAR AHORA</button>
-                </form>
-            </div>
+            <ContactSec/>
+            
         </div>
     );
 }

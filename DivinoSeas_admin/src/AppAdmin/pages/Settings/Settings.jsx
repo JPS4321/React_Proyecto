16b61fe77f4cd8496d968519a93a6ae5043014a3
Joaquin Campos <98112929+JoaquinCampos22155@users.noreply.github.com>
2024-05-23 09:28:01 -0600

    Settings

---
+++ DivinoSeas_admin/src/AppAdmin/pages/Settings/Settings.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../../Componentsadmins/Navbar/Navbar';
+import Contenido from '../../Componentsadmins/Contenido/Contenido';
+import "./SettingsPage.css"; 
+
+const Settings = () => {
+    return (
+      <div className='SettingsPage'>
+          <Navbar />
+          <Contenido />
+      </div>
+    );
+}
+export default Sales;

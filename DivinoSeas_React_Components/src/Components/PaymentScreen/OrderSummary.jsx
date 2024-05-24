aff87f477e232e72276808bba4af408cae0ea9ac
MENENDEZGUERRA <menendezguerrajuan@gmail.com>
2024-05-24 07:22:33 -0600

    Added the payment screen folder structure and two test compoents

---
+++ DivinoSeas_React_Components/src/Components/PaymentScreen/OrderSummary.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const OrderSummary = () => {
+  return (
+    <div className="order-summary">
+      {/* Display the order details here */}
+      <p>Item: T-Shirt</p>
+      <p>Price: $25.00</p>
+      {/* Total calculation */}
+    </div>
+  );
+};
+
+export default OrderSummary;
---
+++ DivinoSeas_React_Components/src/Components/PaymentScreen/PaymentHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PaymentHeader = () => {
+  return (
+    <header className="payment-header">
+      <h1>Payment Information</h1>
+      {/* Cosas de navegación por acá */}
+    </header>
+  );
+};
+
+export default PaymentHeader;

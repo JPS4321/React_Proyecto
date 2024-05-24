7aaad555e23584f8048f6188c9e506f748bc9e4f
Hayser8 <gar22076@uvg.edu.gt>
2024-05-24 03:33:10 -0600

    Componente para mostrar la lista de inventario

---
+++ DivinoSeas_admin/src/AppAdmin/Componentsadmins/ProductInventoryList.jsx
@@ -0,0 +1,25 @@
+import "../styles/Card.css";
+
+function ProductItem({ imageSrc, title, sizes }) {
+  return (
+    <div className="product-item">
+      <div className="product-info">
+        <span className="product-title">{title}</span>
+        <img src={imageSrc} alt={title} className="product-image" />
+      </div>
+      <div className="product-sizes-container">
+        <div className="product-sizes">
+          {sizes.map((size, index) => (
+            <span key={index} className="size-item">
+              <span>{size.count}</span>
+              <span>{size.label}</span>
+            </span>
+          ))}
+        </div>
+      </div>
+      <button className="add-button">+</button>
+    </div>
+  );
+}
+
+export default ProductItem;

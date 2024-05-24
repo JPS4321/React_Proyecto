c3dbf1125508cca129346565b1481031579e70c9
Hayser8 <gar22076@uvg.edu.gt>
2024-05-24 03:32:59 -0600

    cambio al contenido.jsx se agregó un diciconario para mostrar una lista de inventario

--- DivinoSeas_admin/src/AppAdmin/Componentsadmins/Contenido.jsx
+++ DivinoSeas_admin/src/AppAdmin/Componentsadmins/Contenido.jsx
@@ -1,14 +1,42 @@
 import "../styles/Contenido.css";
-import Card from './Card';
+import ProductItem from "./ProductInventoryList.jsx"
 
 const Contenido = () => {
+  const items = [
+    {
+      imageSrc: "https://placehold.co/150x150",
+      title: "Ana de Bretaña Blue atoll TOP",
+      sizes: [
+        { count: 7, label: "XS" },
+        { count: 8, label: "S" },
+        { count: 5, label: "M" },
+        { count: 3, label: "L" },
+      ],
+    },
+    {
+      imageSrc: "https://placehold.co/150x150",
+      title: "Ana de Bretaña Blue atoll BOTTOM",
+      sizes: [
+        { count: 7, label: "XS" },
+        { count: 8, label: "S" },
+        { count: 5, label: "M" },
+        { count: 3, label: "L" },
+      ],
+    },
+  ];
 
   return (
-    <div className="contenido">
-      <Card />
-      
+    <div className="container mx-auto p-4">
+      {items.map((item, index) => (
+        <ProductItem
+          key={index}
+          imageSrc={item.imageSrc}
+          title={item.title}
+          sizes={item.sizes}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Contenido
+export default Contenido;

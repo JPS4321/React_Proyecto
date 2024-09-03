5296ee8b1984e1bb46cb225378892650eca94bdc
Hayser8 <gar22076@uvg.edu.gt>
2024-09-02 20:50:55 -0600

    corrección index y ruta de usuarios

--- api/index.js
+++ api/index.js
@@ -14,6 +14,7 @@ import paymentRouter from "./routes/paymentRoute.js";
 import orderRouter from "./routes/orderRoute.js"; 
 import orderDetailRouter from "./routes/orderDetailRoute.js";
 import userRouter from "./routes/userRoutes.js";
+import clientRouter from "./routes/clientRoute.js"
 
 const app = express();
 
@@ -37,6 +38,7 @@ app.use("/pagos", paymentRouter);
 app.use("/ordenes", orderRouter)
 app.use("/ordenes-detalles", orderDetailRouter)
 app.use("/usuarios", userRouter);
+app.use("/clientes", clientRouter)
 
 app.get("/", (req, res) => {
   res.send("API de Productos Bikini");

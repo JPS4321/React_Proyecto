c086b22b302aa162156a81a3a0e6f753c444034c
MENÉNDEZ GUERRA <67031016+MENENDEZGUERRA@users.noreply.github.com>
2024-09-02 18:34:17 -0600

    Modify Address fields

--- DivinoSeas_React_Components/src/Components/PaymentScreen/ShippingForm.jsx
+++ DivinoSeas_React_Components/src/Components/PaymentScreen/ShippingForm.jsx
@@ -3,9 +3,19 @@ import React from 'react';
 const ShippingForm = () => {
   return (
     <form className="shipping-form">
-      <input type="text" placeholder="First Name" />
-      <input type="text" placeholder="Last Name" />
-      <input type="text" placeholder="Address" />
+      <input type="text" placeholder="First Name" required />
+      <input type="text" placeholder="Last Name" required />
+
+      <input type="text" placeholder="Avenida" required />
+      <input type="text" placeholder="Número de Casa/Apartamento" required />
+      <input type="text" placeholder="Colonia" required />
+
+      <select required>
+        <option value="">Seleccionar Zona</option>
+        {[...Array(22)].map((_, i) => (
+          <option key={i} value={`zona ${i + 1}`}>{`Zona ${i + 1}`}</option>
+        ))}
+      </select>
     </form>
   );
 };

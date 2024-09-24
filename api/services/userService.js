5b36368103164550ad1fb12738f1b60f3d6fbcd7
Hayser8 <gar22076@uvg.edu.gt>
2024-09-23 23:19:36 -0600

    cambio en los servicios de usuario

--- api/services/userService.js
+++ api/services/userService.js
@@ -1,4 +1,5 @@
 import conn from '../connection.js';
+import bcrypt from "bcrypt";
 
 export async function getAllUsers() {
     try {
@@ -22,9 +23,18 @@ export async function getUserById(id_user) {
 
 export async function createUser(username, email, password_hashed, is_admin, role, imagen) {
     try {
+        // Verificar si el correo ya existe
+        const [existingUser] = await conn.query('SELECT * FROM Users WHERE email = ?', [email]);
+        if (existingUser.length > 0) {
+            return { success: false, error: 'El correo ya está registrado' };
+        }
+
+        // Hashear la contraseña
+        const hashedPassword = await bcrypt.hash(password_hashed, 10);
+
         const [result] = await conn.query(
             'INSERT INTO Users (username, email, password_hashed, is_admin, role, imagen) VALUES (?, ?, ?, ?, ?, ?)', 
-            [username, email, password_hashed, is_admin, role, imagen]
+            [username, email, hashedPassword, is_admin, role, imagen]
         );
         return { success: true, result };
     } catch (e) {
@@ -32,6 +42,17 @@ export async function createUser(username, email, password_hashed, is_admin, rol
         return { success: false, error: e };
     }
 }
+
+export async function getUserByEmail(email) {
+    try {
+        const [rows] = await conn.query('SELECT * FROM Users WHERE email = ?', [email]);
+        return rows[0] || null; // Retorna el usuario si lo encuentra, o null si no
+    } catch (e) {
+        console.log(e);
+        return e;
+    }
+}
+
 
 export async function updateUser(id_user, username, email, password_hashed, is_admin, role, imagen) {
     try {

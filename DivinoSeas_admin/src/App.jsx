481f824ace9d31b28aef784d242c81894b441b8a
Hayser8 <gar22076@uvg.edu.gt>
2024-09-23 22:42:08 -0600

    cambio a la app

--- DivinoSeas_admin/src/App.jsx
+++ DivinoSeas_admin/src/App.jsx
@@ -1,22 +1,28 @@
+import React from 'react';
 import Home from './AppAdmin/pages/MainPage/MainPageAdmin';
 import Stock from './AppAdmin/pages/Stock/StockPage';
 import Sales from './AppAdmin/pages/Sales/SalesPage';
 import Settings from './AppAdmin/pages/Settings/SettingsPage';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import LoginPage from './AppAdmin/pages/Login/LoginPage';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import PrivateRoute from './PrivateRoute';
+import AuthProvider from './AuthContext';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />        
-        <Route path="/Stock" element={<Stock />} />        
-        <Route path="/Sales" element={<Sales />} />
-        <Route path="/Settings" element={<Settings />} />         
-
-      </Routes>
-    </Router>
-  )
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/Login" element={<LoginPage />} />
+          <Route path="/Home" element={<PrivateRoute element = {<Home />} />} />
+          <Route path="/Stock" element={<PrivateRoute element={<Stock />} />} />
+          <Route path="/Sales" element={<PrivateRoute element={<Sales />} />} />
+          <Route path="/Settings" element={<PrivateRoute element={<Settings />} />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;

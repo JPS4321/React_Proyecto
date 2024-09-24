18b334568455ae630f6f93c8eb2206b5403a3538
Hayser8 <gar22076@uvg.edu.gt>
2024-09-23 23:19:42 -0600

    authcontext modificado

--- DivinoSeas_admin/src/AuthContext.jsx
+++ DivinoSeas_admin/src/AuthContext.jsx
@@ -1,13 +1,26 @@
 import { createContext, useState } from 'react';
+
 export const AuthContext = createContext();
+
 const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const login = () => setIsAuthenticated(true);
-  const logout = () => setIsAuthenticated(false);
+  const [user, setUser] = useState(null); // Aquí almacenaremos los datos del usuario
+
+  const login = (userData) => {
+    setIsAuthenticated(true);
+    setUser(userData); // Guardamos los datos del usuario al hacer login
+  };
+
+  const logout = () => {
+    setIsAuthenticated(false);
+    setUser(null); // Al cerrar sesión, limpiamos los datos del usuario
+  };
+
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+    <AuthContext.Provider value={{ isAuthenticated, user, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
 };
+
 export default AuthProvider;

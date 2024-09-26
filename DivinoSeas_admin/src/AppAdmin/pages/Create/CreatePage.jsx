4dee608182810efd5cd367315625811a42313984
Joaquin Campos <98112929+JoaquinCampos22155@users.noreply.github.com>
2024-09-26 14:56:49 -0600

    PaginaCrear para admin

---
+++ DivinoSeas_admin/src/AppAdmin/pages/Create/CreatePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../../Componentsadmins/Navbar';
+import ContenidoCreate from '../../Componentsadmins/ContenidoCreate';
+
+const CreatePage = () => {
+  return (
+    <div>
+      <Navbar />
+      <ContenidoCreate />
+    </div>
+  );
+};
+
+export default CreatePage;

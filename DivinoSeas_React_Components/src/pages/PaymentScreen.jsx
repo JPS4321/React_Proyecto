b44b8e378b5becd2a8b2e9a8cfd407074f74fe47
MENÉNDEZ GUERRA <67031016+MENENDEZGUERRA@users.noreply.github.com>
2024-09-26 21:15:10 -0600

    Discount System for Payment Screen

--- DivinoSeas_React_Components/src/pages/PaymentScreen.jsx
+++ DivinoSeas_React_Components/src/pages/PaymentScreen.jsx
@@ -9,19 +9,24 @@ import PaymentOptions from '../Components/PaymentScreen/PaymentOptions';
 import Navbar from '../Components/NavBar/Navbar';
 
 const PaymentScreen = () => {
+  // Ejemplo hardcodeado
+  const price = 200; // Precio de ejemplo
+  const discount = 5; // Ejemplo de porcentaje de descuento
+
   return (
     <div className="container">
-<Navbar/>
-    <div className="payment-screen">
-      <PaymentHeader />
-      <div className="payment-content">
-        <PaymentOptions />
-        <ContactForm />
-        <ShippingForm />
-        <OrderSummary />
-        <PromoCodeInput />
+      <Navbar />
+      <div className="payment-screen">
+        <PaymentHeader />
+        <div className="payment-content">
+          <PaymentOptions />
+          <ContactForm />
+          <ShippingForm />
+          {/* Passing price and discount to OrderSummary */}
+          <OrderSummary price={price} discount={discount} />
+          <PromoCodeInput />
+        </div>
       </div>
-    </div>
     </div>
   );
 };

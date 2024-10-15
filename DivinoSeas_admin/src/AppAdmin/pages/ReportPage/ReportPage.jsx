17de4561f0a257a41b9777eade93a249ee422b2c
Joaquin Campos <98112929+JoaquinCampos22155@users.noreply.github.com>
2024-10-14 19:30:42 -0600

    implementacionespara el reporte

---
+++ DivinoSeas_admin/src/AppAdmin/pages/ReportPage/ReportPage.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../../Componentsadmins/Navbar";
+import { Line } from 'react-chartjs-2';
+
+const ReportPage = () => {
+    return (
+      <div className='ReportPage'>
+          <Navbar />
+      </div>
+    );
+}
+export default ReportPage;

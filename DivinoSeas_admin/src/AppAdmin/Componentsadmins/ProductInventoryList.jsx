890c32c049b0a3c3dbb7f08a702f44903dff9125
sofigo1010 <110646163+sofigo1010@users.noreply.github.com>
2024-11-01 20:11:25 -0600

    cambios a clases para que tengan nombres especificos y no generales

--- DivinoSeas_admin/src/AppAdmin/Componentsadmins/ProductInventoryList.jsx
+++ DivinoSeas_admin/src/AppAdmin/Componentsadmins/ProductInventoryList.jsx
@@ -1,26 +1,26 @@
 import "../styles/Card.css";
 
-function ProductItem({ imageSrc, title, sizes, onEdit }) {
+function ProductItem({ imageSrc, title, sizes }) {
   return (
     <>
-      <div className="product-item">
-        <div className="product-info">
-          <span className="product-title">{title}</span>
-          <img src={imageSrc || "https://via.placeholder.com/150"} alt={title} className="product-image" />
+      <div className="custom-product-item">
+        <div className="custom-product-info">
+          <span className="custom-product-title">{title}</span>
+          <img src={imageSrc} alt={title} className="custom-product-image" />
         </div>
-        <div className="product-sizes-container">
-          <div className="product-sizes">
+        <div className="custom-product-sizes-container">
+          <div className="custom-product-sizes">
             {sizes.map((size, index) => (
-              <span key={index} className="size-item">
+              <span key={index} className="custom-size-item">
                 <span>{size.count}</span>
                 <span>{size.label}</span>
               </span>
             ))}
           </div>
         </div>
-        <button className="add-button" onClick={onEdit}>+</button> {/* Llamar a la función onEdit */}
+        <button className="custom-add-button">+</button>
       </div>
-      <div className="lineapuessfek"></div> 
+      <div className="custom-line-separator"></div> 
     </>
   );
 }

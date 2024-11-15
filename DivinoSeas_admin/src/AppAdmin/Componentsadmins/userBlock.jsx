943864778b0c0dd53f1e7d977fdbe2bbee62077d
Joaquin Campos <98112929+JoaquinCampos22155@users.noreply.github.com>
2024-11-15 01:47:59 -0600

    blockuser

---
+++ DivinoSeas_admin/src/AppAdmin/Componentsadmins/userBlock.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import {
+  puedeAdministrarUsuarios,
+  puedeVerReportes,
+  puedeEditarInventario,
+  puedeCrear,
+} from '../../Roles'; 
+
+const UserBlock = ({ element, requiredPermission, role }) => {
+  const hasPermission = (() => {
+    switch (requiredPermission) {
+      case 'administrarUsuarios':
+        return puedeAdministrarUsuarios(role);
+      case 'verReportes':
+        return puedeVerReportes(role);
+      case 'editarInventario':
+        return puedeEditarInventario(role);
+      case 'crear':
+        return puedeCrear(role);
+      default:
+        return false;
+    }
+  })();
+
+  // Si no tiene permiso, redirige a una página de acceso denegado o inicio
+  return hasPermission ? element : <Navigate to="/access-denied" />;
+};
+
+export default UserBlock;
